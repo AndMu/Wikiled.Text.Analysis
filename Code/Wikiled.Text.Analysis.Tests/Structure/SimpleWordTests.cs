3b1f42355b479e4e4487026320facb38a6a7dcbf
Andrius Mudinas <keistokas@gmail.com>
2017-05-12 11:59:14 +0100

    Adding test moc

---
+++ Code/Wikiled.Text.Analysis.Tests/Structure/SimpleWordTests.cs
@@ -0,0 +1,30 @@
+using NUnit.Framework;
+using System;
+using Wikiled.Text.Analysis.Structure;
+
+namespace Wikiled.Text.Analysis.Tests.Structure
+{
+    [TestFixture]
+    public class SimpleWordTests
+    {
+        private SimpleWord instance;
+
+        [SetUp]
+        public void Setup()
+        {
+            instance = CreateSimpleWord();
+        }
+
+        [Test]
+        public void Construct()
+        {
+            Assert.Throws<ArgumentNullException>(() => new SimpleWord(null));
+            
+        }
+
+        private SimpleWord CreateSimpleWord()
+        {
+            return new SimpleWord("Word");
+        }
+    }
+}
