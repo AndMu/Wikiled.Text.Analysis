df711344d85a91c1e58da69d666184aba3240d0b
Andrius Mudinas <keistokas@gmail.com>
2017-08-22 23:13:35 +0100

    Make vector thread safe again

--- src/Wikiled.Text.Analysis/NLP/NRC/SentimentVector.cs
+++ src/Wikiled.Text.Analysis/NLP/NRC/SentimentVector.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using Wikiled.Text.Analysis.Reflection;
 using Wikiled.Text.Analysis.Reflection.Data;
 
@@ -64,21 +65,22 @@ namespace Wikiled.Text.Analysis.NLP.NRC
 
         [InfoField("Trust")]
         public int Trust => trust;
-     
+
+
         public IEnumerable<Tuple<ItemProbability<string>, ItemProbability<string>>> GetOccurencePairs()
         {
             yield return new Tuple<ItemProbability<string>, ItemProbability<string>>(
-                new ItemProbability<string>("Joy") {Probability = Joy},
-                new ItemProbability<string>("Sadness") {Probability = Sadness});
+                new ItemProbability<string>("Joy") { Probability = Joy },
+                new ItemProbability<string>("Sadness") { Probability = Sadness });
             yield return new Tuple<ItemProbability<string>, ItemProbability<string>>(
-                new ItemProbability<string>("Anger") {Probability = Anger},
-                new ItemProbability<string>("Fear") {Probability = Fear});
+                new ItemProbability<string>("Anger") { Probability = Anger },
+                new ItemProbability<string>("Fear") { Probability = Fear });
             yield return new Tuple<ItemProbability<string>, ItemProbability<string>>(
-                new ItemProbability<string>("Trust") {Probability = Trust},
-                new ItemProbability<string>("Disgust") {Probability = Disgust});
+                new ItemProbability<string>("Trust") { Probability = Trust },
+                new ItemProbability<string>("Disgust") { Probability = Disgust });
             yield return new Tuple<ItemProbability<string>, ItemProbability<string>>(
-                new ItemProbability<string>("Anticipation") {Probability = Anticipation},
-                new ItemProbability<string>("Surprise") {Probability = Surprise});
+                new ItemProbability<string>("Anticipation") { Probability = Anticipation },
+                new ItemProbability<string>("Surprise") { Probability = Surprise });
         }
 
         public IEnumerable<ItemProbability<string>> GetOccurences()
@@ -93,7 +95,7 @@ namespace Wikiled.Text.Analysis.NLP.NRC
 
         public void ExtractData(NRCRecord record)
         {
-            total++;
+            Interlocked.Increment(ref total);
             if (record == null)
             {
                 return;
@@ -103,68 +105,68 @@ namespace Wikiled.Text.Analysis.NLP.NRC
             if (record.IsAnger)
             {
                 added = true;
-                anger++;
+                Interlocked.Increment(ref anger);
             }
 
             if (record.IsAnticipation)
             {
                 added = true;
-                anticipation++;
+                Interlocked.Increment(ref anticipation);
             }
 
             if (record.IsDisgust)
             {
                 added = true;
-                disgust++;
+                Interlocked.Increment(ref disgust);
             }
 
             if (record.IsFear)
             {
                 added = true;
-                fear++;
+                Interlocked.Increment(ref fear);
             }
 
             if (record.IsJoy)
             {
                 added = true;
-                joy++;
+                Interlocked.Increment(ref joy);
             }
 
             if (record.IsSadness)
             {
                 added = true;
-                sadness++;
+                Interlocked.Increment(ref sadness);
             }
 
             if (record.IsSurprise)
             {
                 added = true;
-                surprise++;
+                Interlocked.Increment(ref surprise);
             }
 
             if (record.IsTrust)
             {
                 added = true;
-                trust++;
+                Interlocked.Increment(ref trust);
             }
 
             if (added)
             {
-                totalSentiment++;
+                Interlocked.Increment(ref totalSentiment);
             }
         }
 
         private IEnumerable<ItemProbability<string>> GetOccurencesInternal(double divisor = 1)
         {
             List<ItemProbability<string>> list = new List<ItemProbability<string>>();
-            list.Add(new ItemProbability<string>("Anger") {Probability = Anger / divisor});
-            list.Add(new ItemProbability<string>("Anticipation") {Probability = Anticipation / divisor});
-            list.Add(new ItemProbability<string>("Disgust") {Probability = Disgust / divisor});
-            list.Add(new ItemProbability<string>("Fear") {Probability = Fear / divisor});
-            list.Add(new ItemProbability<string>("Joy") {Probability = Joy / divisor});
-            list.Add(new ItemProbability<string>("Sadness") {Probability = Sadness / divisor});
-            list.Add(new ItemProbability<string>("Surprise") {Probability = Surprise / divisor});
-            list.Add(new ItemProbability<string>("Trust") {Probability = Trust / divisor});
+            list.Add(new ItemProbability<string>("Anger") { Probability = Anger / divisor });
+            list.Add(new ItemProbability<string>("Anticipation") { Probability = Anticipation / divisor });
+            list.Add(new ItemProbability<string>("Disgust") { Probability = Disgust / divisor });
+            list.Add(new ItemProbability<string>("Fear") { Probability = Fear / divisor });
+            list.Add(new ItemProbability<string>("Joy") { Probability = Joy / divisor });
+            list.Add(new ItemProbability<string>("Sadness") { Probability = Sadness / divisor });
+            list.Add(new ItemProbability<string>("Surprise") { Probability = Surprise / divisor });
+            list.Add(new ItemProbability<string>("Trust") { Probability = Trust / divisor });
             return list.OrderBy(item => item.Data);
         }
     }
