403893281a8e751b126c52e67b3c541c1b65ac93
Andrius Mudinas <keistokas@gmail.com>
2018-05-18 21:33:24 +0100

    Fix broken build

--- src/Wikiled.Text.Analysis/Cache/LocalCacheFactory.cs
+++ src/Wikiled.Text.Analysis/Cache/LocalCacheFactory.cs
@@ -1,12 +1,22 @@
-﻿using Wikiled.Text.Analysis.POS;
+﻿using Microsoft.Extensions.Caching.Memory;
+using Wikiled.Common.Arguments;
+using Wikiled.Text.Analysis.POS;
 
 namespace Wikiled.Text.Analysis.Cache
 {
     public class LocalCacheFactory : ICacheFactory
     {
+        private readonly IMemoryCache cache;
+
+        public LocalCacheFactory(IMemoryCache cache)
+        {
+            Guard.NotNull(() => cache, cache);
+            this.cache = cache;
+        }
+
         public ICachedDocumentsSource Create(POSTaggerType tagger)
         {
-            return new LocalDocumentsCache();
+            return new LocalDocumentsCache(cache);
         }
     }
 }
